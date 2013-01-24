c9cf18413d8f013e9bb9c96efa33c476fcfa7a6e
Eliot Kaplan <eliotkaplan@westminster.net>
2013-01-24 16:27:25 -0500

    Added fibonacci to Z.java

--- Z.java
+++ Z.java
@@ -5,4 +5,9 @@ public class Z
     public static void main(String[] args){
       System.out.println("Git this!");
     } /* main */
+	
+	public static void fib(int n) {
+		if (n <= 1) return 1;
+		else return fib(n-1) + fib(n-2);
+	}
 } /* Z */
