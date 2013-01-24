daf7a5dae29e7c54b6eb8fb7dab9de37f73c444f
Bobby Krupczak <rdk@krupczak.org>
2013-01-24 16:28:33 -0500

    Added constructor method

--- Z.java
+++ Z.java
@@ -2,7 +2,21 @@
 
 public class Z
 {
-    public static void main(String[] args){
-      System.out.println("Git this!");
+    public static void main(String[] args)
+    {
+	Z aZ;
+
+        System.out.println("Git this!");
+
+        aZ = new Z(10);
+      
     } /* main */
+
+    int a;
+
+    public Z(int anInt)
+    {   
+	a = anInt;
+    }
+
 } /* Z */
